41cb0615f29893e26dbe257d233b2a79c8bac6a7
zhutmost <zhutmost@outlook.com>
2025-02-07 11:35:00 +0800

    fix: remove unused temp file

--- src/lib/content-collections/slug-path-gen.ts
+++
@@ -1,9 +0,0 @@
-import { Meta } from '@content-collections/core'
-import slugify from '@sindresorhus/slugify'
-
-export default function slugPathGenerate(document: { _meta: Meta }): string {
-  return document._meta.path
-    .split('/')
-    .map((part) => slugify(part)) // remove special chars
-    .join('/')
-}
