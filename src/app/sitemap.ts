f044c12f78d3fec5b5f12aa00f77142f7441780b
zhutmost <zhutmost@outlook.com>
2025-05-19 13:49:46 +0800

    fix: add general page into sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,6 +1,7 @@
 import { MetadataRoute } from 'next'
 
-import { allAuthors } from '@/content-collections'
+import { allPages } from '@/content-collections'
+import { allAuthorsNonDefault, authorDefault } from '@/lib/author-sort'
 import allPostsSorted from '@/lib/post-sort'
 import siteConfig from '@/lib/site-config'
 
@@ -24,24 +25,31 @@ export default function sitemap(): Sitemap {
       changeFrequency: 'monthly',
     }))
 
-  const authorRoutes: Sitemap = allAuthors.map((author) => ({
+  const aboutRoute: SitemapSingleFile = {
+    url: new URL('/about', siteConfig.siteUrl).toString(),
+    lastModified: authorDefault.dateUpdate.toISOString(),
+    priority: 0.7,
+    changeFrequency: 'monthly',
+  }
+
+  const authorsRoutes: Sitemap = allAuthorsNonDefault.map((author) => ({
     url: new URL(`/about/${author.slug}`, siteConfig.siteUrl).toString(),
     lastModified: author.dateUpdate.toISOString(),
     priority: 0.5,
     changeFrequency: 'monthly',
   }))
 
-  const pages = ['news', 'tags']
-  if (siteConfig.teamPage) {
-    pages.push('team')
-  }
-
-  const pageRoutes: Sitemap = pages.map((route) => ({
+  const pagesRoutes: Sitemap = [
+    'news',
+    'tags',
+    ...(siteConfig.teamPage ? ['team'] : []),
+    ...allPages.map((p) => p.slug),
+  ].map((route) => ({
     url: new URL(route, siteConfig.siteUrl).toString(),
     lastModified: new Date().toISOString(),
     priority: 0.5,
     changeFrequency: 'monthly',
   }))
 
-  return [homeRoute, ...postRoutes, ...authorRoutes, ...pageRoutes]
+  return [homeRoute, aboutRoute, ...postRoutes, ...authorsRoutes, ...pagesRoutes]
 }
