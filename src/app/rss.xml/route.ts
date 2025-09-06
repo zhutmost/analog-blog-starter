42ca40fa4c1bf2c54937ce41b9896440a292d4d1
zhutmost <zhutmost@outlook.com>
2025-09-07 00:25:33 +0800

    feat: make rss.xml force-static

--- src/app/rss.xml/route.ts
+++ src/app/rss.xml/route.ts
@@ -1,10 +1,12 @@
 import generateRssFeed from '@/lib/rss'
 
+export const dynamic = 'force-static'
+
 export async function GET() {
   const feed = generateRssFeed()
   return new Response(feed.rss2(), {
     headers: {
-      'Content-Type': 'text/xml',
+      'Content-Type': 'application/rss+xml',
     },
   })
 }
