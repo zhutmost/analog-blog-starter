f186db146fa77c869af241d32d7c5f5f9641ab54
zhutmost <zhutmost@outlook.com>
2025-09-09 22:53:55 +0800

    feat: use fully-bundled shiki for better language support

--- src/components/mdx/shiki-adapter.tsx
+++ src/components/mdx/shiki-adapter.tsx
@@ -1,40 +1,18 @@
 import type * as React from 'react'
 import { CodeBlock, createShikiAdapter } from '@chakra-ui/react'
-import themeGithubDark from '@shikijs/themes/github-dark'
-import themeGithubLight from '@shikijs/themes/github-light'
-import { createHighlighterCore } from 'shiki/core'
-import { createOnigurumaEngine } from 'shiki/engine/oniguruma'
+import { bundledLanguages, createHighlighter, createOnigurumaEngine } from 'shiki'
 
 const shikiAdapter = createShikiAdapter({
   async load() {
-    return createHighlighterCore({
-      langs: [
-        import('@shikijs/langs/c'),
-        import('@shikijs/langs/cpp'),
-        import('@shikijs/langs/dockerfile'),
-        import('@shikijs/langs/html'),
-        import('@shikijs/langs/java'),
-        import('@shikijs/langs/javascript'),
-        import('@shikijs/langs/json'),
-        import('@shikijs/langs/make'),
-        import('@shikijs/langs/markdown'),
-        import('@shikijs/langs/python'),
-        import('@shikijs/langs/scala'),
-        import('@shikijs/langs/shellscript'),
-        import('@shikijs/langs/system-verilog'),
-        import('@shikijs/langs/tcl'),
-        import('@shikijs/langs/typescript'),
-        import('@shikijs/langs/verilog'),
-        import('@shikijs/langs/xml'),
-        import('@shikijs/langs/yaml'),
-      ],
-      themes: [themeGithubDark, themeGithubLight],
+    return createHighlighter({
+      langs: Object.keys(bundledLanguages),
+      themes: ['one-light', 'one-dark-pro'],
       engine: createOnigurumaEngine(import('shiki/wasm')),
     })
   },
   theme: {
-    light: 'github-light',
-    dark: 'github-dark',
+    light: 'one-light',
+    dark: 'one-dark-pro',
   },
 })
 
