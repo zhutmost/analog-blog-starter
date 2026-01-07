c882c6ad7fb01cde93e93738159e69d494870fd9
zhutmost <zhutmost@outlook.com>
2026-01-07 23:15:55 +0800

    fix: process mdxJsxFlowElement correctly when count used languages for shiki

--- src/coco/rehype-code-language-stats.ts
+++ src/coco/rehype-code-language-stats.ts
@@ -1,4 +1,6 @@
 import type { Root } from 'hast'
+import type { MdxJsxAttribute, MdxJsxFlowElement } from 'mdast-util-mdx-jsx'
+import { is } from 'unist-util-is'
 import { type VisitorResult, visit } from 'unist-util-visit'
 
 export interface RehypeCodeLanguageStatsOptions {
@@ -7,13 +9,19 @@ export interface RehypeCodeLanguageStatsOptions {
 
 export default function rehypeCodeLanguageStats({ codeLanguages }: RehypeCodeLanguageStatsOptions) {
   return (tree: Root) => {
-    visit(tree, 'element', (node): VisitorResult => {
-      // return
-      if (node.tagName === 'pre') {
-        // get 'language' property directly (assuming rehype-pre-language has run)
-        const lang = (node.properties?.language as string) || 'plaintext'
+    visit(tree, (node): VisitorResult => {
+      if (is(node, { type: 'mdxJsxFlowElement', name: 'pre' })) {
+        // After the <pre> block is processed by rehypeMdxCodeProps, it will be converted to MdxJsxFlowElement
+        const preNode = node as MdxJsxFlowElement
 
-        // update global statics
+        const lang =
+          (preNode.attributes.find((attr) => (attr as MdxJsxAttribute).name === 'language')
+            ?.value as string) ?? 'plaintext'
+
+        codeLanguages.add(lang)
+      } else if (is(node, { type: 'element', tagName: 'pre' })) {
+        // For raw <pre> blocks
+        const lang = (node.properties?.language as string) || 'plaintext'
         codeLanguages.add(lang)
       }
     })
