f91d6d862832effbb952630501860adabab5f137
zhutmost <zhutmost@outlook.com>
2026-03-09 01:28:02 +0800

    feat: use variable font weights

--- src/lib/font.ts
+++ src/lib/font.ts
@@ -1,18 +1,24 @@
 import { Geist_Mono, Noto_Sans_SC, Space_Grotesk } from 'next/font/google'
 
 const fontSpaceGrotesk = Space_Grotesk({
-  subsets: ['latin'],
   variable: '--font-space-grotesk',
+  weight: 'variable',
+  subsets: ['latin'],
+  display: 'swap',
 })
 
 const fontGeistMono = Geist_Mono({
-  subsets: ['latin'],
   variable: '--font-geist-mono',
+  weight: 'variable',
+  subsets: ['latin'],
+  display: 'swap',
 })
 
 const fontNotoSansSC = Noto_Sans_SC({
-  subsets: ['latin'],
   variable: '--font-noto-sans-sc',
+  weight: 'variable',
+  subsets: ['latin'],
+  display: 'swap',
 })
 
 const customFontFamily = {
