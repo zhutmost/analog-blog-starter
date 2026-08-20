b9119a3d244ff73f4acff0a58e1bfb205ebd6acd
Haozhe Zhu <zhutmost@outlook.com>
2026-08-20 22:27:34 +0800

    fix: fix wrong link tables in nav (#131)

--- sites/blog/site.config.ts
+++ sites/blog/site.config.ts
@@ -23,7 +23,7 @@ const config: InputSiteConfig = {
       { label: "Blog", href: "/posts" },
       { label: "News", href: "/news" },
       { label: "Publication", href: "/publication" },
-      { label: "Publication", href: "/team" },
+      { label: "Team", href: "/team" },
       { label: "About", href: "/author/haozhe-zhu" },
     ],
   },
